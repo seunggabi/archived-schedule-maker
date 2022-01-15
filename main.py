b86e9d3bd38ca0de83d21bbfdfea797851ce5c84
seunggabi <seunggabi.kim@navercorp.com>
2022-01-15 22:12:08 +0900

    feat(#1): refactoring

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ list = [
     {"name": "김다윤", "fix": [0, 1, 2, 5, 6], "o": [], "x": [3, 4], "cnt": 5},
     {"name": "조예진", "fix": [0, 1, 3, 4, 6], "o": [], "x": [2, 5], "cnt": 5},
     {"name": "이재준", "fix": [1, 2, 3, 4, 5], "o": [], "x": [0, 6], "cnt": 5},
-    {"name": "김도량", "fix": [1, 2, 3, 6], "o": [], "x": [0, 3, 4], "cnt": 4},
+    {"name": "김도량", "fix": [1, 2, 5, 6], "o": [], "x": [0, 3, 4], "cnt": 4},
     {"name": "김초연", "fix": [0, 1, 2, 3], "o": [], "x": [4, 5, 6], "cnt": 4},
     {"name": "박선정", "fix": [2, 3], "o": [], "x": [0, 1, 4, 5, 6], "cnt": 2},
     {"name": "최재인", "fix": [6], "o": [], "x": [5], "cnt": 4},
@@ -18,10 +18,10 @@ list = [
     {"name": "임세빈", "fix": [], "o": [], "x": [5, 6], "cnt": 5},
     {"name": "황윤정", "fix": [3, 4, 5], "o": [], "x": [], "cnt": 3},
     {"name": "정다희", "fix": [], "o": [], "x": [0, 2], "cnt": 5},
-    {"name": "김태식", "fix": [0, 4, 5, 6], "o": [], "x": [], "cnt": 4},
+    {"name": "김태식", "fix": [0, 4, 5, 6], "o": [], "x": [1, 2, 3], "cnt": 4},
     {"name": "조민경", "fix": [5], "o": [], "x": [2, 6], "cnt": 5},
     {"name": "문지연", "fix": [], "o": [], "x": [5, 6], "cnt": 5},
-    {"name": "이재형", "fix": [], "o": [], "x": [3, 5, 6], "cnt": 3},
+    {"name": "이재형", "fix": [], "o": [], "x": [3, 6], "cnt": 3},
     {"name": "표예진", "fix": [5], "o": [], "x": [3, 6], "cnt": 3},
     {"name": "김미정", "fix": [5, 6], "o": [], "x": [], "cnt": 5},
 ]
@@ -86,7 +86,7 @@ def pretty(obj):
 
 
 def print_day_work(m):
-    print("### schedule")
+    print("### work")
     for i in sorted(m.keys()):
         print(day[i] + ": " + str(m[i]))
 
@@ -114,6 +114,7 @@ if __name__ == "__main__":
         print(config["max"] * len(day.keys()))
         break
 
+    print("")
     print_day_work(m)
-    print("\n")
-    print_day_remain(m)
+    print("")
+    print_day_rest(m)
