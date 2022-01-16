d1fbfc2349ed6d742e1e5a5cd4b41cdead157a8e
seunggabi <seunggabi.kim@navercorp.com>
2022-01-16 12:31:26 +0900

    feat(#2): work each

--- main.py
+++ main.py
@@ -5,25 +5,25 @@ day = {0: "Mon", 1: "Tue", 2: "Wed", 3: "Thu", 4: "Fri", 5: "Sat", 6: "Sun"}
 config = {"max": 11}
 
 list = [
-    {"name": "문주희", "fix": [], "o": [], "x": [0, 5, 6], "cnt": 3},
     {"name": "김다윤", "fix": [0, 1, 2, 5, 6], "o": [], "x": [3, 4], "cnt": 5},
     {"name": "조예진", "fix": [0, 1, 3, 4, 6], "o": [], "x": [2, 5], "cnt": 5},
     {"name": "이재준", "fix": [1, 2, 3, 4, 5], "o": [], "x": [0, 6], "cnt": 5},
     {"name": "김도량", "fix": [1, 2, 5, 6], "o": [], "x": [0, 3, 4], "cnt": 4},
     {"name": "김초연", "fix": [0, 1, 2, 3], "o": [], "x": [4, 5, 6], "cnt": 4},
     {"name": "박선정", "fix": [2, 3], "o": [], "x": [0, 1, 4, 5, 6], "cnt": 2},
+    {"name": "이선민", "fix": [6], "o": [], "x": [5], "cnt": 4},
+    {"name": "문주희", "fix": [], "o": [], "x": [0, 5, 6], "cnt": 3},
     {"name": "최재인", "fix": [6], "o": [], "x": [5], "cnt": 4},
     {"name": "조윤지", "fix": [6], "o": [], "x": [5], "cnt": 5},
-    {"name": "이선민", "fix": [6], "o": [], "x": [5], "cnt": 4},
     {"name": "임세빈", "fix": [], "o": [], "x": [5, 6], "cnt": 5},
     {"name": "황윤정", "fix": [3, 4, 5], "o": [], "x": [], "cnt": 3},
     {"name": "정다희", "fix": [], "o": [], "x": [0, 2], "cnt": 5},
     {"name": "김태식", "fix": [0, 4, 5, 6], "o": [], "x": [1, 2, 3], "cnt": 4},
-    {"name": "조민경", "fix": [5], "o": [], "x": [2, 6], "cnt": 5},
+    {"name": "조민경", "fix": [5], "o": [], "x": [2, 3, 6], "cnt": 4},
     {"name": "문지연", "fix": [], "o": [], "x": [5, 6], "cnt": 5},
     {"name": "이재형", "fix": [], "o": [], "x": [3, 6], "cnt": 3},
     {"name": "표예진", "fix": [5], "o": [], "x": [3, 6], "cnt": 3},
-    {"name": "김미정", "fix": [5, 6], "o": [], "x": [], "cnt": 5},
+    {"name": "김미정", "fix": [5, 6], "o": [], "x": [], "cnt": 3},
 ]
 
 
@@ -97,6 +97,23 @@ def print_day_rest(m):
         print(day[i] + ": " + str([x for x in all() if x not in m[i]]))
 
 
+def print_day_work_each(m):
+    print("### work each")
+
+    map = {}
+
+    for i in m:
+        for k in m[i]:
+            if k not in map:
+                map[k] = []
+
+            map[k].append(i)
+
+    for i in sorted(map.keys()):
+        map[i].sort()
+        print(i + "(" + str(len(map[i])) + "): " + ','.join([day[x] for x in map[i]]))
+
+
 def all():
     return [x["name"] for x in list]
 
@@ -118,3 +135,5 @@ if __name__ == "__main__":
     print_day_work(m)
     print("")
     print_day_rest(m)
+    print("")
+    print_day_work_each(m)
